cce60821d879ac7687a218f5a4a87787c95473c3
Max <maksymilianrozanski@gmail.com>
2020-09-08 17:33:03 +0200

    change correct answer type

--- src/main/scala/exams/data/Exam.scala
+++ src/main/scala/exams/data/Exam.scala
@@ -5,6 +5,6 @@ sealed trait Exam
 final case class EmptyExam(questions: List[BlankQuestion]) extends Exam
 final case class CompletedExam(questions: List[BlankQuestion]) extends Exam
 
-case class Question(question: BlankQuestion, correctAnswer: Answer)
-case class BlankQuestion(text: String, answers: List[Answer], selectedAnswer: Option[Answer])
+case class Question(question: BlankQuestion, correctAnswer: List[Int])
+case class BlankQuestion(text: String, answers: List[Answer], selectedAnswer: List[Int])
 case class Answer(text: String)
