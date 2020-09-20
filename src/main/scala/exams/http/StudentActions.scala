2f44bcae7c63b902e1633e55f85b8c860f739871
Max <maksymilianrozanski@gmail.com>
2020-09-20 18:37:26 +0200

    remove TestCommand from StudentActions.scala

--- src/main/scala/exams/http/StudentActions.scala
+++ src/main/scala/exams/http/StudentActions.scala
@@ -9,7 +9,6 @@ import exams.data.StudentsExam
 object StudentActions {
 
   sealed trait Command
-  final case class TestCommand(replyTo: ActorRef[ActionPerformed]) extends Command
   final case class RequestExamCommand(code: String, replyTo: ActorRef[ExamToDisplay], distributor: ActorRef[ExamDistributor]) extends Command
 
   final case class ActionPerformed(description: String)
@@ -20,10 +19,6 @@ object StudentActions {
   def registry(): Behavior[Command] = {
     Behaviors.setup(context =>
       Behaviors.receiveMessage {
-        case TestCommand(replyTo) =>
-          context.log.info("received test command")
-          replyTo ! ActionPerformed("test action performed!")
-          Behaviors.same
         case RequestExamCommand(code, displayReceiver, distributor) =>
           context.log.info("received starting exam request")
           val student = context.spawnAnonymous(Student(displayReceiver))
