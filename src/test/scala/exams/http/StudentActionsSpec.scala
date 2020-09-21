1de7eefd18c80b13b250fe49f03a52bb0ad40a28
Max <maksymilianrozanski@gmail.com>
2020-09-21 14:12:40 +0200

    add StudentActions test

--- src/test/scala/exams/http/StudentActionsSpec.scala
+++ src/test/scala/exams/http/StudentActionsSpec.scala
@@ -1,7 +1,12 @@
 package exams.http
 
+
+import akka.actor.testkit.typed.Effect.SpawnedAnonymous
 import akka.actor.testkit.typed.scaladsl.{BehaviorTestKit, TestInbox}
-import exams.ExamDistributor.{ExamDistributor, RequestExamEvaluationCompact}
+import akka.actor.typed.ActorRef
+import exams.ExamDistributor.{ExamDistributor, RequestExam, RequestExamEvaluationCompact}
+import exams.Student
+import exams.http.StudentActions.{ExamToDisplay, RequestExamCommand}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
 
@@ -13,5 +18,21 @@ class StudentActionsSpec extends AnyFlatSpec with should.Matchers {
     val messageContent = RequestExamEvaluationCompact("exam123", List(List("1"), List("2", "3")))
     testKit.run(StudentActions.SendExamToEvaluation(messageContent))
     inbox.expectMessage(messageContent)
+  }
+
+  "StudentActions" should "send spawn Student and send message to ExamDistributor" in {
+    //setup
+    val inbox = TestInbox[ExamDistributor]()
+    val displayInbox = TestInbox[ExamToDisplay]()
+    //given
+    val message = RequestExamCommand("123", displayInbox.ref)
+    val testKit = BehaviorTestKit(StudentActions()(inbox.ref))
+    //when
+    testKit.run(message)
+    //then
+    val spawnedAnonymous = testKit.expectEffectType[SpawnedAnonymous[Student]]
+
+    val expectedMessage = RequestExam("123", spawnedAnonymous.ref)
+    inbox.expectMessage(expectedMessage)
   }
 }
