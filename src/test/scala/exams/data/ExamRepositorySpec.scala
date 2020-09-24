782258c15e464f9617055c6eb633872d61504861
Max <maksymilianrozanski@gmail.com>
2020-09-24 19:06:25 +0200

    optimize imports

--- src/test/scala/exams/data/ExamRepositorySpec.scala
+++ src/test/scala/exams/data/ExamRepositorySpec.scala
@@ -1,12 +1,12 @@
 package exams.data
 
-import akka.actor.testkit.typed.scaladsl.{LogCapturing, ScalaTestWithActorTestKit}
+import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
 import akka.actor.typed.scaladsl.{ActorContext, Behaviors}
 import akka.persistence.testkit.scaladsl.EventSourcedBehaviorTestKit
 import akka.persistence.testkit.scaladsl.EventSourcedBehaviorTestKit.SerializationSettings.disabled
 import akka.persistence.typed.PersistenceId
 import akka.persistence.typed.scaladsl.EventSourcedBehavior
-import exams.data.ExamRepository.{AddQuestionsSet, ExamRepository, ExamRepositoryEvents, ExamRepositoryState, QuestionsSet, QuestionsSetAdded, examRepository}
+import exams.data.ExamRepository.{AddQuestionsSet, ExamRepository, ExamRepositoryEvents, ExamRepositoryState, QuestionsSet, QuestionsSetAdded}
 import org.scalatest.wordspec.AnyWordSpecLike
 
 class ExamRepositorySpec extends ScalaTestWithActorTestKit(EventSourcedBehaviorTestKit.config) with AnyWordSpecLike {
@@ -28,7 +28,6 @@ class ExamRepositorySpec extends ScalaTestWithActorTestKit(EventSourcedBehaviorT
   private val question3 = Question(BlankQuestion("question 3 text", List(Answer("yes"), Answer("no"))), List())
   private val question4 = Question(BlankQuestion("new question", List(Answer("yes"), Answer("no"))), List(Answer("no")))
   "ExamRepository" when {
-
     val persistedSets = List(
       QuestionsSet("1", "test set1", Set(question1, question2)),
       QuestionsSet("2", "test set2", Set(question3)))
