65b752d9aba777dae06d9a6c7e5a7ad585ae5613
Max <maksymilianrozanski@gmail.com>
2020-09-26 14:02:23 +0200

    add ReceivedExamRequest message handling

--- src/main/scala/exams/data/ExamGenerator.scala
+++ src/main/scala/exams/data/ExamGenerator.scala
@@ -2,8 +2,8 @@ package exams.data
 
 import akka.actor.typed.{ActorRef, Behavior}
 import akka.actor.typed.scaladsl.Behaviors
-import exams.ExamDistributor.ExamId
-import exams.data.ExamRepository.{ExamRepository, QuestionsSet, SetId, TakeQuestionsSet}
+import exams.ExamDistributor.{ExamDistributor, ExamId}
+import exams.data.ExamRepository.{ExamRepository, QuestionsSet, SetId, TakeQuestionsSet, TakeQuestionsSetReply}
 
 object ExamGenerator {
 
@@ -15,4 +15,36 @@ object ExamGenerator {
     TeachersExam(id, List(q1, q2))
   }
 
+  sealed trait ExamGenerator
+  //incoming msg from ExamDistributor
+  final case class ReceivedExamRequest(examRequest: ExamRequest) extends ExamGenerator
+
+ final case class ReceivedSetFromRepo(set: TakeQuestionsSetReply) extends ExamGenerator
+
+  case class State(requests: Set[ExamRequest])
+
+  def apply(repository: ActorRef[ExamRepository])(distributor: ActorRef[ExamDistributor])(state: State): Behavior[ExamGenerator] =
+    generator(repository)(distributor)(state)
+
+  def generator(repository: ActorRef[ExamRepository])(distributor: ActorRef[ExamDistributor])(state: State): Behavior[ExamGenerator] =
+    Behaviors.setup { context =>
+      val responseMapper: ActorRef[TakeQuestionsSetReply] =
+        context.messageAdapter(response => ReceivedSetFromRepo(response))
+
+      Behaviors.receiveMessage {
+        case ReceivedExamRequest(examRequest@ExamRequest(examId, _, _, setId)) =>
+          val newState = state.copy(requests = state.requests + examRequest)
+          repository ! TakeQuestionsSet(setId, examId, responseMapper)
+          generator(repository)(distributor)(newState)
+        case ReceivedSetFromRepo(set) =>
+          //get ExamRequest from current state
+
+          //create TeachersExam
+
+          //send TeachersExam to ExamDistributor
+
+          //remove ExamRequest from state
+          ???
+      }
+    }
 }
--- src/test/scala/exams/data/ExamGeneratorSpec.scala
+++ src/test/scala/exams/data/ExamGeneratorSpec.scala
@@ -1,17 +1,53 @@
 package exams.data
 
-import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
-import akka.persistence.testkit.scaladsl.EventSourcedBehaviorTestKit
+import akka.actor.testkit.typed.scaladsl.{BehaviorTestKit, TestInbox}
+import exams.ExamDistributor.ExamDistributor
+import exams.data.ExamGenerator.{ReceivedExamRequest, State}
+import exams.data.ExamRepository.{ExamRepository, TakeQuestionsSet}
 import org.scalatest.wordspec.AnyWordSpecLike
 
-class ExamGeneratorSpec extends ScalaTestWithActorTestKit(EventSourcedBehaviorTestKit.config) with AnyWordSpecLike {
+class ExamGeneratorSpec extends AnyWordSpecLike {
 
   "ExamGenerator" when {
+    val repository = TestInbox[ExamRepository]()
+    val distributor = TestInbox[ExamDistributor]()
 
     "receive ExamRequest" should {
+      val examRequest = ExamRequest("exam123", "student123", 2, "set2")
+      val message = ReceivedExamRequest(examRequest)
 
-      "send message to ExamRepository" in {
+      val testKit = BehaviorTestKit(ExamGenerator(repository.ref)(distributor.ref)(State(Set.empty[ExamRequest])))
+      testKit.run(message)
 
+      "send message to repository, and message received by repository" should {
+        val receivedByRepository = repository.receiveMessage()
+
+        "contain correct setId" in {
+          receivedByRepository match {
+            case TakeQuestionsSet(setId, _, _) =>
+              assertResult(examRequest.setId)(setId)
+            case _ => fail(s"$receivedByRepository should match to TakeQuestionsSet")
+          }
+        }
+
+        "contain correct examId" in {
+          receivedByRepository match {
+            case TakeQuestionsSet(_, examId, _) => assertResult(examRequest.examId)(examId)
+            case _ => fail(s"$receivedByRepository should match to TakeQuestionsSet")
+          }
+        }
+
+        "contain ref to ExamGenerator" in {
+          receivedByRepository match {
+            case TakeQuestionsSet(_, _, replyTo) =>
+              val testMessage = ("testExamId", None)
+              //sending message to replyTo passed to TakeQuestionsSet should be received by testKit
+              replyTo ! testMessage
+              val receivedAsString = testKit.selfInbox().receiveAll().head.toString
+              assert(receivedAsString.contains(s"${testMessage._1},${testMessage._2}"))
+            case _ => fail(s"$receivedByRepository should match to TakeQuestionsSet")
+          }
+        }
       }
 
       "add ExamRequest to state" in {
