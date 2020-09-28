9883dc8b77f35fdf64b63daaaaef77235d94b418
Max <maksymilianrozanski@gmail.com>
2020-09-28 15:33:41 +0200

    use AnyWordSpecLike in StudentActionsSpec

--- src/test/scala/exams/http/StudentActionsSpec.scala
+++ src/test/scala/exams/http/StudentActionsSpec.scala
@@ -3,17 +3,43 @@ package exams.http
 
 import akka.actor.testkit.typed.Effect.SpawnedAnonymous
 import akka.actor.testkit.typed.scaladsl.{BehaviorTestKit, TestInbox}
-import akka.actor.typed.ActorRef
 import exams.ExamDistributor.{ExamDistributor, RequestExam, RequestExam2, RequestExamEvaluation}
 import exams.data.{Answer, StudentsRequest}
 import exams.http.StudentActions.{ExamToDisplay, RequestExamCommand, RequestExamCommand2}
 import exams.student.Student
-import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
+import org.scalatest.wordspec.AnyWordSpecLike
 
-class StudentActionsSpec extends AnyFlatSpec with should.Matchers {
+class StudentActionsSpec extends AnyWordSpecLike with should.Matchers {
 
-  "StudentActions" should "redirect message to ExamDistributor" in {
+  "StudentActions" when {
+    "receive RequestExamCommand" should {
+      val distributor = TestInbox[ExamDistributor]()
+      val displayInbox = TestInbox[ExamToDisplay]()
+      val message = RequestExamCommand("123", displayInbox.ref)
+      val testKit = BehaviorTestKit(StudentActions()(distributor.ref))
+      testKit.run(message)
+      "send spawn Student and send message to ExamDistributor" in {
+        val spawnedAnonymous = testKit.expectEffectType[SpawnedAnonymous[Student]]
+        val expectedMessage = RequestExam("123", spawnedAnonymous.ref)
+        distributor.expectMessage(expectedMessage)
+      }
+    }
+    "receive RequestExamCommand2" should {
+      val distributor = TestInbox[ExamDistributor]()
+      val displayInbox = TestInbox[ExamToDisplay]()
+      val studentsRequest = StudentsRequest("student12", 3, "set3")
+      val message = RequestExamCommand2(studentsRequest, displayInbox.ref)
+      val testKit = BehaviorTestKit(StudentActions()(distributor.ref))
+      testKit.run(message)
+      "send spawn Student and send message to ExamDistributor when receive RequestExamCommand2" in {
+        val spawnedAnonymous = testKit.expectEffectType[SpawnedAnonymous[Student]]
+        val expectedMessage = RequestExam2(studentsRequest, spawnedAnonymous.ref)
+        distributor.expectMessage(expectedMessage)
+      }
+    }
+  }
+  "receive RequestExamEvaluation command" should {
     val inbox = TestInbox[ExamDistributor]()
     val testKit = BehaviorTestKit(StudentActions()(inbox.ref))
     val messageContent = RequestExamEvaluation("exam123",
@@ -22,39 +48,8 @@ class StudentActionsSpec extends AnyFlatSpec with should.Matchers {
         List(Answer("2"), Answer("3"))
       ))
     testKit.run(StudentActions.SendExamToEvaluation(messageContent))
-    inbox.expectMessage(messageContent)
-  }
-
-  "StudentActions" should "send spawn Student and send message to ExamDistributor" in {
-    //setup
-    val distributor = TestInbox[ExamDistributor]()
-    val displayInbox = TestInbox[ExamToDisplay]()
-    //given
-    val message = RequestExamCommand("123", displayInbox.ref)
-    val testKit = BehaviorTestKit(StudentActions()(distributor.ref))
-    //when
-    testKit.run(message)
-    //then
-    val spawnedAnonymous = testKit.expectEffectType[SpawnedAnonymous[Student]]
-
-    val expectedMessage = RequestExam("123", spawnedAnonymous.ref)
-    distributor.expectMessage(expectedMessage)
-  }
-
-  "StudentActions" should "send spawn Student and send message to ExamDistributor when receive RequestExamCommand2" in {
-    //setup
-    val distributor = TestInbox[ExamDistributor]()
-    val displayInbox = TestInbox[ExamToDisplay]()
-    //given
-    val studentsRequest = StudentsRequest("student12", 3, "set3")
-    val message = RequestExamCommand2(studentsRequest, displayInbox.ref)
-    val testKit = BehaviorTestKit(StudentActions()(distributor.ref))
-    //when
-    testKit.run(message)
-    //then
-    val spawnedAnonymous = testKit.expectEffectType[SpawnedAnonymous[Student]]
-
-    val expectedMessage = RequestExam2(studentsRequest, spawnedAnonymous.ref)
-    distributor.expectMessage(expectedMessage)
+    "redirect message to ExamDistributor" in {
+      inbox.expectMessage(messageContent)
+    }
   }
 }
