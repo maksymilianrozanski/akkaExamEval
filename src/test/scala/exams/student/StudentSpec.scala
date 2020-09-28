f689e701a01821db2d7d65dbfc83c8ef13e54dc9
Max <maksymilianrozanski@gmail.com>
2020-09-28 15:00:43 +0200

    add GeneratingExamFailed unit test

--- src/test/scala/exams/student/StudentSpec.scala
+++ src/test/scala/exams/student/StudentSpec.scala
@@ -1,7 +1,8 @@
 package exams.student
 
 import akka.actor.testkit.typed.scaladsl.{BehaviorTestKit, TestInbox}
-import exams.ExamDistributor.{ExamDistributor, RequestExam, RequestExam2}
+import akka.actor.typed.scaladsl.Behaviors
+import exams.ExamDistributor.{ExamDistributor, RequestExam2}
 import exams.data.StudentsRequest
 import exams.http.StudentActions.ExamToDisplay
 import org.scalatest.wordspec.AnyWordSpecLike
@@ -9,15 +10,25 @@ import org.scalatest.wordspec.AnyWordSpecLike
 class StudentSpec extends AnyWordSpecLike {
 
   "Student" when {
-    val distributor = TestInbox[ExamDistributor]()
-    val displayReceiver = TestInbox[ExamToDisplay]()
-    val testKit = BehaviorTestKit(Student(displayReceiver.ref))
     "receive RequestExamCommand" should {
+      val distributor = TestInbox[ExamDistributor]()
+      val displayReceiver = TestInbox[ExamToDisplay]()
+      val testKit = BehaviorTestKit(Student(displayReceiver.ref))
       val studentsRequest = StudentsRequest("student123", 3, "set2")
       val command = RequestExamCommand(studentsRequest, distributor.ref)
       testKit.run(command)
       "send RequestExam message to ExamDistributor" in
         distributor.expectMessage(RequestExam2(command.code, testKit.ref))
+      "have 'same' behavior" in
+        assertResult(Behaviors.same)(testKit.returnedBehavior)
+    }
+
+    "receive GeneratingExamFailed" should {
+      val displayReceiver = TestInbox[ExamToDisplay]()
+      val testKit = BehaviorTestKit(Student(displayReceiver.ref))
+      testKit.run(GeneratingExamFailed)
+      "have 'same' behavior" in
+        assertResult(Behaviors.same)(testKit.returnedBehavior)
     }
   }
 }
