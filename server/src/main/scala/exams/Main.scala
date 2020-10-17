bba4440d3eb2be51e73b0f1a7c7d1a2d71d9ba1d
Max <maksymilianrozanski@gmail.com>
2020-10-17 11:23:58 +0200

    uncomment business logic code in Main

--- server/src/main/scala/exams/Main.scala
+++ server/src/main/scala/exams/Main.scala
@@ -37,19 +37,19 @@ object Main {
 
   def apply(): Behavior[NotUsed] =
     Behaviors.setup { context =>
-//      val examEvaluator = context.spawnAnonymous(ExamEvaluator())
-//      val repository = context.spawnAnonymous(ExamRepository())
-//      val examGenerator = context.spawnAnonymous(ExamGenerator(repository))
-//      val distributorActors = ExamDistributor.ActorsPack(examEvaluator, examGenerator)
-//      implicit val distributor: ActorRef[ExamDistributor] = context.spawn(ExamDistributor(examEvaluator, distributorActors), "distributor")
-//
-//      implicit val studentActions: ActorRef[StudentActions.Command] = context.spawn(StudentActions(), "studentActions")
-//      context.watch(studentActions)
-//      implicit val timeout: Timeout = Timeout.create(context.system.settings.config.getDuration("my-app.routes.ask-timeout"))
-//      implicit val actorPack: RoutesActorsPack = RoutesActorsPack(studentActions, context.system, distributor, repository, examEvaluator, timeout)
-//      val routes = RoutesRoot.createStudentRoutes(actorPack)
+      val examEvaluator = context.spawnAnonymous(ExamEvaluator())
+      val repository = context.spawnAnonymous(ExamRepository())
+      val examGenerator = context.spawnAnonymous(ExamGenerator(repository))
+      val distributorActors = ExamDistributor.ActorsPack(examEvaluator, examGenerator)
+      implicit val distributor: ActorRef[ExamDistributor] = context.spawn(ExamDistributor(examEvaluator, distributorActors), "distributor")
 
-      startHttpServer(testJs, context.system)
+      implicit val studentActions: ActorRef[StudentActions.Command] = context.spawn(StudentActions(), "studentActions")
+      context.watch(studentActions)
+      implicit val timeout: Timeout = Timeout.create(context.system.settings.config.getDuration("my-app.routes.ask-timeout"))
+      implicit val actorPack: RoutesActorsPack = RoutesActorsPack(studentActions, context.system, distributor, repository, examEvaluator, timeout)
+      val routes = RoutesRoot.createStudentRoutes(actorPack)
+
+      startHttpServer(routes, context.system)
 
       Behaviors.receiveSignal {
         case (_, Terminated(_)) => Behaviors.stopped
