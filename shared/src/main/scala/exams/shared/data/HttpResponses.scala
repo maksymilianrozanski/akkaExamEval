6840174956f14470d53b29ed264f4031201cc5f1
Max <maksymilianrozanski@gmail.com>
2020-10-31 12:11:38 +0100

    add doc to ExamResult

--- shared/src/main/scala/exams/shared/data/HttpResponses.scala
+++ shared/src/main/scala/exams/shared/data/HttpResponses.scala
@@ -2,6 +2,13 @@ package exams.shared.data
 
 object HttpResponses {
 
+  /**
+   * Item of an array returned at GET /repo/results endpoint
+   *
+   * @param examId    generated unique exam identifier
+   * @param studentId not unique name provided by user
+   * @param result    score of an exam (0.0 - 1.0)
+   */
   case class ExamResult(examId: String, studentId: String, result: Double)
 
   case class ExamGenerated(exam: StudentsExam)
